99eefa99cdf1191334da8c6859a3bad62be25df1
dgarmash <dgarmash@synergy.ru>
2024-01-15 13:31:39 +0300

    change app object to class

--- app.js
+++ app.js
@@ -1,62 +1,96 @@
-const app = (() => {
-    const app = {};
-
+class App {
     //#region debug
-    let debug = true;
-    /**
-     * @param {boolean} new_debug
-     */
-    app.setDebug = (new_debug) => debug = !!new_debug;
+    _debug = true;
+    set debug(new_debug) {this._debug = !!new_debug};
+    get debug() {return this._debug};
     //#endregion
 
     //#region logs
-    /**
-     * @param {string} prefix 
-     * @returns 
-     */
-    const logger = (prefix) => {
-        const messages = [];
+    _logs = new Map();
+    _log = this.getLogger('App');
+    get loggers() {return Object.fromEntries(this._logs)};
+    /** @param {string} prefix */
+    getLogger(prefix) {
         /**
-         * @param {string} message 
-         * @param {object} data
-         * @returns {{log: function, messages: Array<{time: number, message: string, data: object}>}}
+         * @param {string} message - The message to be logged.
+         * @param {object} [data={}] - Optional data associated with the message.
          */
         const log = (message, data = {}) => {
             messages.push({time: Date.now(), message, data});
-            if(debug) console.log(`${prefix}: ${message}`, data);
-        };
-        return {
-            log,
-            messages,
-        };
-    }    
-    const logs = new Map();
-    app.getLoggers = () => Object.fromEntries(logs);
-    /**
-     * @param {string} prefix
-     */
-    app.getLogger = (prefix) => {
-        const log = logger(prefix);
-        logs.set(prefix, log.messages);
-        return log.log;
+            if(this._debug) console.log(`${prefix}: ${message}`, data);
+        }
+        const messages = [];
+        this._logs.set(prefix, messages);
+        return log;
+    }
+    //#endregion
+
+    //#region setup
+    /** @param {Array<Function>} initializers */
+    ready = (initializers) => {
+        this._log('Setting up initializers...');
+        const fn = () => initializers.map(initializer => {
+            try {
+                this._log(`Running: ${initializer.name}`);
+                initializer();
+                this._log(`Complete: ${initializer.name}`);
+            } catch(error) {
+                this._log(`Error occurred while running initializer: ${initializer.name}`, {error});
+            }
+        });
+
+        document.readyState === 'loading'
+            ? document.addEventListener('DOMContentLoaded', fn)
+            : fn();
     };
     //#endregion
 
-    return app;
-})();
-// app.setDebug(false);
+    loadScript(src) {
+        return new Promise((resolve, reject) => {
+            const script = document.createElement('script');
+            script.src = src;
+            script.onload = resolve;
+            script.onerror = reject;
+            document.body.appendChild(script);
+        });
+    }
+    loadStyles(url) {
+        return new Promise((resolve, reject) => {
+            const link = document.createElement('link');
+            link.rel = 'stylesheet';
+            link.href = url;
+            link.onload = resolve;
+            link.onerror = reject;
+            document.head.appendChild(link);
+        });
+    }
+    _modules = {
+        jquery: {
+            js: [],
+            css: [],
+        }
+    }
+    
+    loadModules(keys) {
 
-const logger = app.getLogger('1');
-logger('1.1');
-logger('1.2');
-logger('1.3');
-logger('1.4');
+    }
+}
+const app = new App();
 
-const logger2 = app.getLogger('2');
-logger2('2.1');
-logger2('2.2');
-logger('1.5');
-logger2('2.3');
-logger2('2.4');
+const initSomething = () => {
+    const log = app.getLogger('initSomething');
+    log('START');
+    setTimeout(() => {
+        log('Something is happening...');
+    }, 2500);
+    log('END');
+}
+const showLogs = () => {
+    if(!app.debug) return;
+    console.log(app.loggers);
+}
 
-console.log(app.getLoggers());
+app.ready([
+    initSomething,
+    showLogs
+]);
